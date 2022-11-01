139c44c0a0070ec6c459b4f9176cfde8e6e8d1c8
VitorFerns <vitorfernz+github@gmail.com>
2022-11-01 12:51:21 -0300

    initial commit

---
+++ RandomPhoUITests/RandomPhoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RandomPhoUITestsLaunchTests.swift
+//  RandomPhoUITests
+//
+//  Created by Vitor Fernandes da Paz on 01/11/22.
+//
+
+import XCTest
+
+class RandomPhoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
